1e0f5db5f8f9cf2a0c9f512a90df68c45f2273c4
Ubuntu <ubuntu@ip-172-31-26-142.ec2.internal>
2022-05-15 01:09:20 +0000

    added

--- index.js
+++ index.js
@@ -8,9 +8,22 @@ const client = new Client({
 client.on('qr', qr => {
     qrcode.generate(qr, {small: true});
 });
-
+client.on('message', message => {
+	if(message.body === '!ping') {
+		message.reply('pong');
+	}
+});
 client.on('ready', () => {
     console.log('Client is ready!');
+});
+client.on('message', message => {
+	console.log(message.body);
+});
+
+client.on('message', message => {
+	if(message.body === '!ping') {
+		message.reply('pong');
+	}
 });
 
 client.initialize();
